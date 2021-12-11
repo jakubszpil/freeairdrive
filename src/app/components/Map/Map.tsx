5bcf30dcb2224f77f7948ac6d6d553af600f0404
Jakub Szpil <46812496+jakubszpil@users.noreply.github.com>
2021-12-11 15:12:56 +0100

    Add files via upload

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -2,54 +2,89 @@ import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import MarkerShadow from "leaflet/dist/images/marker-shadow.png";
 import MarkerIcon from "leaflet/dist/images/marker-icon.png";
-import { icon } from "leaflet";
+
 import "./Map.css";
-import {getPoints, getPolygons} from "app/services/geo";
+import { getPoints, getPolygons } from "app/services/geo";
 import MapSearch from "./MapSearch";
+import { calculateDistance } from "app/services/distance";
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 // @ts-ignore
 L.Icon.Default.mergeOptions({
   iconUrl: MarkerIcon,
-  // shadowUrl: MarkerShadow,
-});
-
-const iconPin = icon({
-  iconUrl: MarkerIcon,
   shadowUrl: MarkerShadow,
-  // iconSize: [24, 54],
-  // shadowSize: [34, 54],
 });
 
-function Polygons({ position, radius }: any) {
+function Polygons({
+  position,
+  radius,
+  setIsSuccessfulFoundRoute,
+}: {
+  position: {
+    latitude: number;
+    longitude: number;
+  } | null;
+  radius: number;
+  setIsSuccessfulFoundRoute: (v: boolean) => void;
+}) {
   const [group, setGroup] = useState<any>(null);
+  const [control, setControl] = useState<any>(null);
   const map = useMap();
+
   useEffect(() => {
-    if (position?.latitude && position?.longitude) {
-      getPoints(map, `around:${radius},50.2606725,19.0224603`).then(r => console.log(r));
-      getPolygons(map, `around:${radius},50.2606725,19.0224603`).then(
-        (polyGroup) => {
-          if (group) {
-            group.removeFrom(map);
-            setGroup(null);
-          }
-          polyGroup.addTo(map);
-          setGroup(polyGroup);
+    if (position) {
+      const { latitude, longitude } = position;
+
+      getPoints(
+        map,
+        `around:${radius},${position?.latitude},${position?.longitude}`,
+        latitude,
+        longitude
+      ).then((r) => {
+        console.log(r);
+        if (control) {
+          map.removeControl(control);
+          setControl(null);
         }
-      );
+        if (r.shortestPath) {
+          calculateDistance(
+            [latitude, longitude],
+            [r.shortestPath.latitude, r.shortestPath.longitude]
+          ).then((distanceControl) => {
+            distanceControl.addTo(map);
+            setGroup(distanceControl);
+            setIsSuccessfulFoundRoute(true);
+          });
+        }
+      });
+      getPolygons(
+        map,
+        `around:${radius},${position.latitude},${position.longitude}`
+      ).then((polyGroup) => {
+        if (group && "removeFrom" in group) {
+          group?.removeFrom(map);
+          setGroup(null);
+        }
+        polyGroup.addTo(map);
+        setGroup(polyGroup);
+      });
     }
   }, [map, position, radius]);
 
   return null;
 }
 
-function Map() {
+function Map({
+  setIsSuccessfulFoundRoute,
+}: {
+  setIsSuccessfulFoundRoute: (v: boolean) => void;
+}) {
   const [position, setPosition] = useState<{
     latitude: number;
     longitude: number;
   } | null>(null);
 
-  const [radius, setRadius] = useState(200);
+  const [radius, setRadius] = useState(1000);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -58,6 +93,19 @@ function Map() {
         longitude: position.coords.longitude,
       });
     });
+
+    // const watchPositionNumber = navigator.geolocation.watchPosition(
+    //   (position) => {
+    //     setPosition({
+    //       latitude: position.coords.latitude,
+    //       longitude: position.coords.longitude,
+    //     });
+    //   }
+    // );
+
+    // return () => {
+    //   navigator.geolocation.clearWatch(watchPositionNumber);
+    // };
   }, [setPosition]);
 
   if (!position) {
@@ -67,7 +115,7 @@ function Map() {
   return (
     <div className="map">
       <MapContainer
-        center={[50.2606725, 19.0224603]}
+        center={[position.latitude, position.longitude]}
         zoom={13}
         scrollWheelZoom={true}
       >
@@ -78,12 +126,16 @@ function Map() {
           }
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[50.2606725, 19.0224603]} icon={iconPin}>
+        <Marker position={[position.latitude, position.longitude]}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            <h3>You are here!</h3>
           </Popup>
         </Marker>
-        <Polygons position={position} radius={radius} />
+        <Polygons
+          position={position}
+          radius={radius}
+          setIsSuccessfulFoundRoute={setIsSuccessfulFoundRoute}
+        />
         <MapSearch setRadius={setRadius} radius={radius} />
       </MapContainer>
     </div>

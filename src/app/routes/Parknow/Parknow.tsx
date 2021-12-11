4e60137b3ed723b27b9ee8798833502f3f65cf46
Jakub Szpil <46812496+jakubszpil@users.noreply.github.com>
2021-12-11 15:04:16 +0100

    Add files via upload

--- src/app/routes/Parknow/Parknow.tsx
+++ src/app/routes/Parknow/Parknow.tsx
@@ -1,9 +1,54 @@
-import './Parknow.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Map from "app/components/Map";
+import { useState } from "react";
+import { Alert, Container } from "react-bootstrap";
+import "./Parknow.css";
+
+library.add(fas);
 
 function Parknow() {
+  const [isSuccesfulFoundRoute, setIsSuccessfulFoundRoute] = useState(false);
+
   return (
-    <div className="parknow">
-      Parknow dzialaj plis
+    <div className="parknow ">
+      <Container className="mx-auto" fluid="lg">
+        <Map setIsSuccessfulFoundRoute={setIsSuccessfulFoundRoute} />
+
+        {isSuccesfulFoundRoute ? (
+          <div className="d-flex align-items-center justify-content-between">
+            <h1 className="mb-4 py-5">Park now</h1>
+            <div className="d-flex">
+              <div className="d-flex flex-column p-2 m-2">
+                <div className="d-flex justify-content-between">
+                  <FontAwesomeIcon size="lg" icon={["fas", "bicycle"]} />
+                  {Math.floor(Math.random() * 10)}
+                </div>
+                <p>Electric scooter: </p>
+              </div>
+              <div className="d-flex flex-column p-2 m-2">
+                <div className="d-flex justify-content-between">
+                  <FontAwesomeIcon size="lg" icon={["fas", "bicycle"]} />
+                  {Math.floor(Math.random() * 10)}
+                </div>
+                <p>Electric bike</p>
+              </div>
+              <div className="d-flex flex-column p-2 m-2">
+                <div className="d-flex justify-content-between">
+                  <FontAwesomeIcon size="lg" icon={["fas", "motorcycle"]} />
+                  {Math.floor(Math.random() * 10)}
+                </div>
+                <p>Electric motor scooter</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <Alert>Change radius of parking area to start parking</Alert>
+          </>
+        )}
+      </Container>
     </div>
   );
 }

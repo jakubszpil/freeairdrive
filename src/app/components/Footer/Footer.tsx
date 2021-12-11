9b6862ac9e554ac6ca3655f01636444fcef40e77
Kacper Grela <38227679+kgrela@users.noreply.github.com>
2021-12-11 13:51:07 +0100

    t commit message

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,41 +2,41 @@ import { Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import './Footer.css';
-import {APP_PATHS} from 'app/contants';
+import { APP_PATHS } from 'app/contants';
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            <Nav>
-                <div className='footer-left'>
-                   <LinkContainer to={APP_PATHS.TEAM}>
-                    <Nav.Link >Team</Nav.Link>
-                   </LinkContainer>
-                   <LinkContainer to={APP_PATHS.CAREER}>
-                    <Nav.Link >Carrers</Nav.Link>
-                   </LinkContainer>
-                   <LinkContainer to={APP_PATHS.NEWS_ROOM}>
-                    <Nav.Link >News room</Nav.Link>
-                   </LinkContainer>
-                </div>
-                <div className='footer-center'>
-                    <h2>awPark</h2>
-                    <p>Copyright &copy; 2021</p>
-                </div>
-                <div className='footer-right'>
-                <LinkContainer to={APP_PATHS.LOGIN}>
-                    <Nav.Link >Log in</Nav.Link>
-                   </LinkContainer>
-                   <LinkContainer to={APP_PATHS.SIGNUP}>
-                    <Nav.Link >Sign out</Nav.Link>
-                   </LinkContainer>
-                   <LinkContainer to={APP_PATHS.SUPPORT}>
-                    <Nav.Link >Support</Nav.Link>
-                   </LinkContainer>
-                </div>
-            </Nav>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <Nav>
+        <div className="footer-left">
+          <LinkContainer to={APP_PATHS.TEAM}>
+            <Nav.Link>Team</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to={APP_PATHS.CAREER}>
+            <Nav.Link>Carrers</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to={APP_PATHS.NEWS_ROOM}>
+            <Nav.Link>News room</Nav.Link>
+          </LinkContainer>
+        </div>
+        <div className="footer-center">
+          <h2>awPark</h2>
+          <p>Copyright &copy; 2021</p>
+        </div>
+        <div className="footer-right">
+          <LinkContainer to={APP_PATHS.LOGIN}>
+            <Nav.Link>Log in</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to={APP_PATHS.SIGNUP}>
+            <Nav.Link>Sign up</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to={APP_PATHS.SUPPORT}>
+            <Nav.Link>Support</Nav.Link>
+          </LinkContainer>
+        </div>
+      </Nav>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/app/routes/Cooperation/Cooperation.tsx
+++ src/app/routes/Cooperation/Cooperation.tsx
@@ -1,46 +1,97 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './Cooperation.css';
-import {ReactComponent as Image} from "images/cooperation.svg";
+import { ReactComponent as Image } from 'images/cooperation.svg';
+import { Alert, Button, FloatingLabel, Form } from 'react-bootstrap';
 
 function Cooperation() {
-    const [nameValue, setNameValue] = useState('');
-    const [emailValue, setEmailValue] = useState('');
-    const [textAreaValue, setTextAreaValue] = useState('');
+  const [nameValue, setNameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [textAreaValue, setTextAreaValue] = useState('');
 
-    const handleSubmit = () => {
-        console.log("HARDO");
-    };
+  const handleSubmit = () => {
+    console.log('HARDO');
+  };
 
-    return (
-        <div className="cooperation">
-            <h2 className="cooperation-header">
-                Are you carpark owner? <br/>
-                Do you provide electric vehicles?
-            </h2>
-            <div className='cooperation-top'>
-                <div className='cooperation-left'>
-                    <h1>We are proud to invite <br/> business clients to <br/> cooperate with us!</h1>
-                    <p>By working with us, you will gain new <br/> customers and a lot of satisfaction with
-                        the <br/> support of air purification!</p>
-                </div>
-                <div className='cooperation-right'>
-                    <Image className='cooperation-image'/>
-                </div>
-            </div>
-            <div>
-                <div className='cooperation-form'>
-                    <h1>Let's talk with us!</h1>
-                    <input type='text' placeholder='Name' value={nameValue}
+  return (
+    <div className="cooperation">
+      <h2 className="cooperation-header">
+        Are you carpark owner? <br />
+        Do you provide electric vehicles?
+      </h2>
+      <div className="cooperation-top">
+        <div className="cooperation-left">
+          <h1 className="text-secondary mb-5 fw-bold">
+            We are proud to invite <br /> business clients to <br /> cooperate
+            with us!
+          </h1>
+          <p>
+            By working with us, you will gain new <br /> customers and a lot of
+            satisfaction with the <br /> support of air purification!
+          </p>
+        </div>
+        <div className="cooperation-right">
+          <Image className="cooperation-image" />
+        </div>
+      </div>
+
+      <Form className="cooperation-form forms-support">
+        <h1 className="text-secondary mb-5 fw-bold">Let's talk with us!</h1>
+        <Form.Group>
+          <FloatingLabel
+            controlId="name"
+            label="Name"
+            className="mb-3 text-center"
+          >
+            <Form.Control placeholder="Enter Your Name here" />
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group>
+          <FloatingLabel
+            controlId="email"
+            label="Email"
+            className="mb-3 text-center"
+          >
+            <Form.Control placeholder="Enter Your Email here" type="email" />
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group>
+          <FloatingLabel
+            controlId="message"
+            label="Message"
+            className="mb-3 text-center"
+          >
+            <textarea className="form-control h-25"></textarea>
+          </FloatingLabel>
+        </Form.Group>
+
+        <div className="mb-3">
+          <Button
+            className="login-form__button p-2 text-uppercase"
+            variant="primary"
+            type="submit"
+          >
+            Send message
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export default Cooperation;
+
+/*
+
+
+                    <input className='' type='text' placeholder='Name' value={nameValue}
                            onChange={(e) => setNameValue(e.target.value)}/>
                     <input type='text' placeholder='Email' value={emailValue}
                            onChange={(e) => setEmailValue(e.target.value)}/>
                     <textarea placeholder='Message' value={textAreaValue}
                               onChange={(e) => setTextAreaValue(e.target.value)}/>
                     <button type='submit' onChange={handleSubmit}>SEND MESSAGE</button>
-                </div>
-            </div>
-        </div>
-    );
-}
 
-export default Cooperation;
+
+*/
